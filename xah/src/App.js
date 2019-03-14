066a853327fdc0b0467010ba28af5198210eefb1
daniyarka <daniyar_k-98@mail.ru>
2019-03-14 16:44:45 +0600

    project reinit

---
+++ xah/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Mainpage from './components/main.js'
+
+class App extends Component {
+  render() {
+    return (
+      < Mainpage/>
+    );
+  }
+}
+
+export default App;
