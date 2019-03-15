7d19c0018f0a09d4f0512b04cb89cfd028a6bb92
NMomyshuly <porcuon19@gmail.com>
2019-03-15 21:29:25 +0600

    category and article

--- han/src/App.js
+++ han/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from '../src/components/header/header'
 import Footer from '../src/components/footer/footer'
+import Category from '../src/components/category/category'
+import Article from '../src/components/article/article'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
+        <Article/>
+        <Category/>
         <Footer/>
       </div>
     );
