b305bc6dfc585c83410ff1ea3e88393ab54109df
NMomyshuly <porcuon19@gmail.com>
2019-03-16 13:01:24 +0600

    changed css

--- han/src/App.js
+++ han/src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Header from '../src/components/header/header'
 import Footer from '../src/components/footer/footer'
 import Category from '../src/components/category/category'
 import Article from '../src/components/article/article'
-=======
-import Header from '../src/components/header/header';
-import Footer from '../src/components/footer/footer';
-import Mainpage from './components/mainpage/main'
->>>>>>> ba6e71f2a1b1e33581a6fbe556c1db4147d912f2
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header/>
-<<<<<<< HEAD
         <Article/>
         <Category/>
-=======
-        <Mainpage/>
->>>>>>> ba6e71f2a1b1e33581a6fbe556c1db4147d912f2
         <Footer/>
       </div>
     );
