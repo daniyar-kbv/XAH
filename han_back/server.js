a1c04afe0097df3ac8da350ed4eab4168baf2508
daniyarka <daniyar_k-98@mail.ru>
2019-05-04 18:13:05 +0600

    cors

--- han_back/server.js
+++ han_back/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+var cors = require('cors')
+app.use(cors())
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const dbConfig = require('./config/database.config.js');
