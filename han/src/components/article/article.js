ccb60481bc50eade4f276e250715dc1c74a23730
porcuon19 <porcuon19@gmail.com>
2019-05-06 18:16:39 +0300

    postInCard refactored

--- han/src/components/article/article.js
+++ han/src/components/article/article.js
@@ -1,48 +1,18 @@
 import React, { Component } from 'react'
 import './article.css'
 import '../../assets/css/theme.css'
-import incard from '../../assets/img/graphic-product-bench-thumb.jpg'
 import bigimage from '../../assets/img/photo-woman-writing.jpg'
 import * as articleActions from '../../actions/articleActions' 
 import { connect } from 'react-redux'
 import Comment from './comment'
-
-class PostInCard extends Component{
-    render(){
-        return(
-            <li>
-                <div className="media">
-                    <a href="#">
-                        <img alt="Image" src={incard} className="avatar avatar-square rounded mr-3" />
-                    </a>
-                    <div className="media-body">
-                        <a href="#">
-                            <span className="h6">Bench</span>
-                        </a>
-                        <span className="badge badge-secondary">Productivity</span>
-                        <div className="text-muted">
-                            <ul className="list-inline">
-                                <li className="list-inline-item">
-                                    <small><i className="icon-heart"></i> 373</small>
-                                </li>
-                                <li className="list-inline-item">
-                                    <small><i className="icon-message"></i> 62</small>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </li>
-        );
-    }
-}
+import PostInCard from '../postincard/postInCard'
 
 class Article extends Component {
 
-    componentDidMount() {
-        this.props.getArticles();
-        console.log(this.props.articles);
-    }
+    // componentDidMount() {
+    //     this.props.getArticles();
+    //     console.log(this.props.articles);
+    // }
 
   render() {
     return (
