9c07c11829f8439c047bf99b0687129117dfedd0
porcuon19 <porcuon19@gmail.com>
2019-05-10 23:12:07 +0600

    login screen

--- han_mobile/App.js
+++ han_mobile/App.js
@@ -15,12 +15,8 @@ import {
   Icon,
   Body,
   Title,
-  List,
-  ListItem,
-  Card,
-  CardItem,
-  Thumbnail
 } from 'native-base';
+import Login from './screens/login/login';
 
 export default class App extends React.Component {
 
@@ -46,41 +42,42 @@ export default class App extends React.Component {
       );
     }
     return (
-      <Container>
-        <Header hasTabs style={{ backgroundColor: '#000000', elevation: 0 }} androidStatusBarColor='#000000'>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>XAH e-Journal</Title>
-          </Body>
-          <Right>
-            <Button transparent>
-              <Text>Вход</Text>
-            </Button>
-          </Right>
-        </Header>
-        <Tabs>
-          <Tab heading="Главная" tabStyle={{backgroundColor: '#000000'}} textStyle={{color: '#fff'}} activeTabStyle={{backgroundColor: '#000000'}} activeTextStyle={{color: '#fff'}}></Tab>
-          <Tab heading="Авто" tabStyle={{backgroundColor: '#000000'}} textStyle={{color: '#fff'}} activeTabStyle={{backgroundColor: '#000000'}} activeTextStyle={{color: '#fff'}}></Tab>
-          <Tab heading="Бизнес" tabStyle={{backgroundColor: '#000000'}} textStyle={{color: '#fff'}} activeTabStyle={{backgroundColor: '#000000'}} activeTextStyle={{color: '#fff'}}></Tab>
-          <Tab heading="Спорт" tabStyle={{backgroundColor: '#000000'}} textStyle={{color: '#fff'}} activeTabStyle={{backgroundColor: '#000000'}} activeTextStyle={{color: '#fff'}}></Tab>
-        </Tabs>
-        <Fab style={{backgroundColor: '#000000'}}>
-          <Icon name="search" />
-        </Fab>
-      </Container>
+      // <Container>
+      //   <Header hasTabs style={{ backgroundColor: '#000000', elevation: 0 }} androidStatusBarColor='#000000'>
+      //     <Left>
+      //       <Button transparent>
+      //         <Icon name='menu' />
+      //       </Button>
+      //     </Left>
+      //     <Body>
+      //       <Title>XAH e-Journal</Title>
+      //     </Body>
+      //     <Right>
+      //       <Button transparent>
+      //         <Text>Вход</Text>
+      //       </Button>
+      //     </Right>
+      //   </Header>
+      //   <Tabs>
+      //     <Tab heading="Главная" tabStyle={{ backgroundColor: '#000000' }} textStyle={{ color: '#fff' }} activeTabStyle={{ backgroundColor: '#000000' }} activeTextStyle={{ color: '#fff' }}></Tab>
+      //     <Tab heading="Авто" tabStyle={{ backgroundColor: '#000000' }} textStyle={{ color: '#fff' }} activeTabStyle={{ backgroundColor: '#000000' }} activeTextStyle={{ color: '#fff' }}></Tab>
+      //     <Tab heading="Бизнес" tabStyle={{ backgroundColor: '#000000' }} textStyle={{ color: '#fff' }} activeTabStyle={{ backgroundColor: '#000000' }} activeTextStyle={{ color: '#fff' }}></Tab>
+      //     <Tab heading="Спорт" tabStyle={{ backgroundColor: '#000000' }} textStyle={{ color: '#fff' }} activeTabStyle={{ backgroundColor: '#000000' }} activeTextStyle={{ color: '#fff' }}></Tab>
+      //   </Tabs>
+      //   <Fab style={{ backgroundColor: '#000000' }}>
+      //     <Icon name="search" />
+      //   </Fab>
+      // </Container>
+      <Login/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
